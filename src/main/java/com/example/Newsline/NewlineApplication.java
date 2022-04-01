9779724c3031077b4ea0983a99d1060fc590c137
Ivan Bespalov <bespalovivan@mail.ru>
2022-04-01 11:56:47 +0700

    First commit

---
+++ src/main/java/com/example/Newsline/NewlineApplication.java
@@ -0,0 +1,11 @@
+package com.example.Newsline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewlineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewlineApplication.class,args);
+    }
+}
